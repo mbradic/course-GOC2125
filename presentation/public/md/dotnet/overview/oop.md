57723296f673776433858ef2cf4deafac44c4382
Miroslav Bradic <mbradic@yahoo.com>
2024-05-28 16:42:37 +0200

    code

--- presentation/public/md/dotnet/overview/oop.md
+++ presentation/public/md/dotnet/overview/oop.md
@@ -1,5 +1,13 @@
+- Jmenný prostor
 - Třída (class)
 - Člen třídy (member)
-    - Pole (field)
-    - Vlastnost (property)
+  - Pole (field)
+  - Vlastnost (property)
+  - Metoda
 - Zapouzdření
+
+## Jmenný prostor (namespace)
+
+```csharp
+namespace Gopas.Courses {}
+```
--- presentation/src/Topic.jsx
+++ presentation/src/Topic.jsx
@@ -1,28 +1,52 @@
 import React, { useEffect, useState } from "react";
+import { CodeBlock, vs2015 } from "react-code-blocks";
 import Markdown from "react-markdown";
 
 export function Topic({ topic }) {
-  const [content, setContent] = useState([])
+  const [content, setContent] = useState([]);
+
   useEffect(() => {
     async function getData() {
-      const result = []
+      const result = [];
       for (const contentItem of topic.content) {
-        const response = await fetch(contentItem.url)
-        const text = await response.text()
-        result.push({ ...contentItem, text })
+        const response = await fetch(contentItem.url);
+        const text = await response.text();
+        result.push({ ...contentItem, text });
       }
-      setContent(result)
+      setContent(result);
     }
-    getData()
-  }, [topic.content])
-  return <div style={{ paddingLeft: "15px" }}>
-    <h3>{topic.title}</h3>
-    {content.map((contentItem, ndx) => <div key={ndx}>{
-      (function() {
-      switch (contentItem.type) {
-        case "md": return <Markdown>{contentItem.text}</Markdown>
-        default: return <p style={{ color: 'red' }}>Error: Unknown contentItem type.</p>
-      }})()
-    }</div>)}
-  </div>;
+    getData();
+  }, [topic.content]);
+
+  return (
+    <div style={{ paddingLeft: "15px" }}>
+      <h3>{topic.title}</h3>
+      {content.map((contentItem, ndx) => (
+        <div key={ndx}>
+          {(function () {
+            switch (contentItem.type) {
+              case "md":
+                return <Markdown>{contentItem.text}</Markdown>;
+              case "cs":
+                return (
+                  <code>
+                    <CodeBlock
+                      text={contentItem.text}
+                      language="csharp"
+                      theme={vs2015}
+                    />
+                  </code>
+                );
+              default:
+                return (
+                  <p style={{ color: "red" }}>
+                    Error: Unknown contentItem type.
+                  </p>
+                );
+            }
+          })()}
+        </div>
+      ))}
+    </div>
+  );
 }
